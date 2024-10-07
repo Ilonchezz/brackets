5decbf74caab1b6d2ea3e424a98005f7d3f9e2f6
Ilona <zaitsavailona@gmail.com>
2024-10-07 17:05:55 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+
+  const isOpening = (bracket) => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (bracket === bracketsConfig[i][0]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const isClosing = (bracket) => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (bracket === bracketsConfig[i][1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const getOpening = (closingBracket) => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (closingBracket === bracketsConfig[i][1]) {
+        return bracketsConfig[i][0];
+      }
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (isClosing(str[i])) {      
+      if (stack[stack.length - 1] !== getOpening(str[i])) {
+        if (isOpening(str[i])) {
+          stack.push(str[i]);
+          continue;
+        }
+        return false;
+      }
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+
+  return stack.length === 0;
 }
